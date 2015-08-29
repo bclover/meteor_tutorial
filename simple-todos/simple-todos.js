5ad1691d28225b4974939e30ec423abd4bcb442b
Bryan Clover <bclover09@gmail.com>
2015-08-29 10:49:29 -0400

    Completed Step 11 of the Meteor.js tutorial. (Added code to limit data and allow privacy settings for the data using publich and subscribe)

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,7 +1,23 @@
 Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+	Meteor.publish('tasks', function () {
+
+		// Only publish tasks that are public or belong to the current user
+		return Tasks.find({
+			$or: [
+				{ private: {$ne: true} },
+				{ owner: this.userId }
+			]
+		});
+	});
+}
+
 /****** CLIENT-SIDE CODE *********/
 if(Meteor.isClient){
+
+	Meteor.subscribe("tasks");
+
 	Template.body.helpers({
 		tasks: function(){
 			if (Session.get("hideCompleted")) {
@@ -22,6 +38,12 @@ if(Meteor.isClient){
 		}
 	});
 
+	Template.task.helpers({
+		isOwner: function () {
+			return this.owner === Meteor.userId();
+		}
+	});
+
 	Template.body.events({
 		"submit .new-task": function(event){
 			//prevent default browser form submit
@@ -35,7 +57,6 @@ if(Meteor.isClient){
 
 			// Clear form
 			event.target.text.value = "";
-
 		},
 
 		"change .hide-completed input": function(event){
@@ -48,6 +69,10 @@ if(Meteor.isClient){
 
 		"click .delete": function(){
 			Meteor.call("deleteTask", this._id);
+		},
+
+		"click .toggle-private": function () {
+			Meteor.call("setPrivate", this._id, !this.private);
 		}
 	});
 
@@ -61,22 +86,52 @@ Meteor.methods({
 
 	addTask: function(text){
 		if(!Meteor.userId()){
-			throw new Meteor.Error("Sorry but you are not authorized to take this action.");
+			throw new Meteor.Error("You are not authorized to take this action.");
 		}
 
 		Tasks.insert({
 			text: text,
 			createdAt: new Date(),
 			owner: Meteor.userId(),
-			username: Meteor.user().username
+			username: Meteor.user().username,
+			private: true
 		});
 	},
 
 	deleteTask: function (taskId) {
-		Tasks.remove(taskId);
+		var task = Tasks.findOne(taskId);
+		if (task.private && task.owner !== Meteor.userId()) {
+			// If the task is private, make sure only the owner can delete it
+			alert("You do not have permission to delete this task.");
+			throw new Meteor.Error("You do not have permission to delete this task.");
+		}
+
+		if(task.owner !== Meteor.userId()){
+			alert("You do not have permission to delete this task.");
+			throw new Meteor.Error("You do not have permission to delete this task.");
+		}else{
+			Tasks.remove(taskId);
+		}
+
 	},
 
 	setChecked: function (taskId, setChecked) {
+		var task = Tasks.findOne(taskId);
+		if (task.private && task.owner !== Meteor.userId()) {
+			// If the task is private, make sure only the owner can check it off
+			alert("You do not have permission to mark this task as complete.");
+			throw new Meteor.Error("You do not have permission to check this task.");
+		}
 		Tasks.update(taskId, { $set: { checked: setChecked} });
+	},
+
+	setPrivate: function(taskId, setToPrivate) {
+		var task = Tasks.findOne(taskId);
+		if(task.owner !== Meteor.userId()) {
+			alert("You do not have permission to set the privacy of this task.");
+			throw new Meteor.Error("You do not have permission to set the privacy of this task.");
+		}
+
+		Tasks.update(taskId, { $set: { private: setToPrivate } });
 	}
 });
